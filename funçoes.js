bb31fffed3e260fdad6d9dfb54b35bec93a1be41
Nicolas-61127 <n.ramos@ba.estudante.senai.br>
2025-05-26 19:44:27 +0000

    m

---
+++ funçoes.js
@@ -0,0 +1,21 @@
+
+
+function subtrair(a, b) {
+    return a - b;
+}
+
+const subtracao = subtrair(2,3)
+console.log(`A subtração é ${subtracao}`); 
+
+
+function somar(a, b) {
+    return a + b;
+}
+const soma = somar(2,3)
+console.log(`A soma é ${soma}`);    
+
+function multiplicar(a, b) {
+    return a * b;
+}
+const multiplicacao = multiplicar(2,3)
+console.log(`A multiplicação é ${multiplicacao}`);
